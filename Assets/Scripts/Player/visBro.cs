f856b7a9e32c42052fc2bc780275f5f24a6eb04f
Sammmojr <sammojr614@gmail.com>
2020-06-09 18:06:48 -0400

    Attempted to Make bros Fuse

--- Assets/Scripts/Player/visBro.cs
+++ Assets/Scripts/Player/visBro.cs
@@ -7,6 +7,7 @@ public class visBro : MonoBehaviour
     public List<GameObject> brothers = new List<GameObject>();
     private void Update()
     {
+        //First Bro
         if (BroSwitch.Bro == 1)
         {
             brothers[0].SetActive(true);
@@ -14,6 +15,7 @@ public class visBro : MonoBehaviour
             brothers[2].SetActive(false);
             PlayerMovement.moveSpeed = 10;
             PlayerMovement.jumpHeight = 9;
+            //Second Bro
         } else if (BroSwitch.Bro == 2)
         {
             brothers[1].SetActive(true);
@@ -21,7 +23,10 @@ public class visBro : MonoBehaviour
             brothers[2].SetActive(false);
             PlayerMovement.moveSpeed = 5;
             PlayerMovement.jumpHeight = 8;
+            //Putting the Bro Into The Pos of the first
+            /*brothers[1].transform.position = brothers[0].transform.position;*/
 
+            //Third Bro
         } else if (BroSwitch.Bro == 3)
         {
             brothers[1].SetActive(false);
@@ -29,6 +34,8 @@ public class visBro : MonoBehaviour
             brothers[2].SetActive(true);
             PlayerMovement.moveSpeed = 2;
             PlayerMovement.jumpHeight = 4;
+            //Putting the 3 bro where the 1st bro was 
+            /*brothers[2].transform.position = brothers[1].transform.position;*/
         }
     }
 
