b92f1264cc5c2d9cc5d8a5648dfabec1f260de2b
Sammmojr <sammojr614@gmail.com>
2020-06-10 13:04:17 -0400

    coming together with a Database

---
+++ Assets/Scripts/DataBasing/Writing/NewData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class NewData : MonoBehaviour
+{
+    public Button CreateButton;
+    DbManager dataMgr = DbManager.Instance;
+    void Start()
+    {
+        CreateButton.onClick.AddListener(CreateOnClick);
+    }
+    void CreateOnClick()
+    {
+        dataMgr.dbHealth = 2;
+        dataMgr.dbLives = 4;
+        dataMgr.dbCoins = 0;
+        string tojson = JsonUtility.ToJson(dataMgr);
+        File.WriteAllText(Application.dataPath + "PlayerSaveData.json", tojson);
+        SceneManager.LoadScene("Testing");
+    }
+
+  
+}
---
+++ Assets/Scripts/U.I/Health.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public static int PlayerHealth;
+    public static int PlayerLives;
+    public List<SpriteRenderer> Hearts = new List<SpriteRenderer>();
+    public Sprite FullHeart;
+    public Sprite HalfHeart;
+    
+    
+    private void Start()
+    {
+        string load = File.ReadAllText(Application.dataPath + "PlayerSaveData.json");
+        DbManager loadHealth = JsonUtility.FromJson<DbManager>(load);
+        PlayerHealth = loadHealth.dbHealth;
+        PlayerLives = loadHealth.dbLives;
+
+    }
+    private void Update()
+    {
+        if(PlayerHealth == 2)
+        {
+            Hearts[0].sprite = FullHeart;
+        }else if(PlayerHealth == 1)
+        {
+            Hearts[0].sprite = HalfHeart;
+        }
+    }
+
+
+
+}
