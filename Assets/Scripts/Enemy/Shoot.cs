769b1cc19a1102b89be9fde40e18f398437fbc7b
Epiczd <nutball37@gmail.com>
2020-06-15 18:11:55 -0400

    Improved Shooting Script

--- Assets/Scripts/Enemy/Shoot.cs
+++ Assets/Scripts/Enemy/Shoot.cs
@@ -8,29 +8,55 @@ public class Shoot : MonoBehaviour
     public GameObject Stud;
     public Sprite LegoHeadIdle;
     public Sprite LegoHeadShoot;
-    Vector3 change = new Vector3(-3f, 0f, 0f);
+
+    //Max distance the enemy can fire
+    public float xMin;
+    public float xMax;
+
+    //Direction the Stud is moving (by default this is left)
+    private Vector3 MovingDirection = Vector3.left;
+
+    //Speed the Stud is moving
+    private float moveSpeed = 5f;
+
     private void Start()
     {
         Stud.SetActive(false);
         Stud.transform.position = LegoHead.transform.position;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
+        //When the player steps within the trigger, the Stud will fire
         if (collision.CompareTag("Player"))
         {
-            Stud.transform.position += change;
+
             LegoHead.sprite = LegoHeadShoot;
             Stud.SetActive(true);
-          
+
+            //Prevents the Stud from going out of bounds
+            if (Stud.transform.position.x >= xMin)
+            {
+                Stud.transform.Translate(MovingDirection * Time.smoothDeltaTime * moveSpeed);
+            }
+            else
+            {
+                Stud.transform.position = LegoHead.transform.position;
+            }
+
 
         }
-        else
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //When the player leaves the range, the legohead will stop shooting
+        if (collision.CompareTag("Player"))
         {
             LegoHead.sprite = LegoHeadIdle;
             Stud.SetActive(false);
             Stud.transform.position = LegoHead.transform.position;
-
         }
     }
 }
