5e8d7298db92996e0d2c925780ad904f00c587d3
Sammmojr <sammojr614@gmail.com>
2020-09-01 11:30:23 -0400

    Camera Follow

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerafollow : MonoBehaviour
+{
+    //Minimum/Maximum x/y values
+    [SerializeField] private float xMin;
+    [SerializeField] private float xMax;
+    [SerializeField] private float yMin;
+    [SerializeField] private float yMax;
+
+    //The player
+    [SerializeField] private Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    //Allows the camera to follow the player when they move
+    void FollowPlayer()
+    {
+        float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+        float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+    }
+}
