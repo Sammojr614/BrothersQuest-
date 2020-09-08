3e065d31a3a59323c060cbc44718812e01d726d0
Sammojr614 <Sammojr614@gmail.com>
2020-09-08 14:09:15 -0400

    added more scenes to Tutorial and added a Hub

---
+++ Assets/Scripts/Player/BroLock.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BroLock : MonoBehaviour
+{
+    public int lockedBro;
+    // Update is called once per frame
+    void Update()
+    {
+        BroSwitch.Bro = lockedBro;
+    }
+}
