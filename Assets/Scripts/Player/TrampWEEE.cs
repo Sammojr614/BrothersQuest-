c1f34ee467a5cc29c3757dd39b714840d37ecb53
Sammojr614 <Sammojr614@gmail.com>
2020-09-08 14:19:55 -0400

    WEEEE

---
+++ Assets/Scripts/Player/TrampWEEE.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampWEEE : MonoBehaviour
+{
+    public GameObject Player;
+    public int FlingHeight;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(0f, FlingHeight, 0f), ForceMode2D.Impulse);
+        }
+    }
+}
