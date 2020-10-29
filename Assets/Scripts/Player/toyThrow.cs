91e9375db8e202fbbfc6f76e4224b9f21c965059
Sammmojr <sammojr614@gmail.com>
2020-10-29 19:39:52 -0400

    Throwin Toys

--- Assets/Scripts/Player/toyThrow.cs
+++ Assets/Scripts/Player/toyThrow.cs
@@ -7,20 +7,31 @@ public class toyThrow : MonoBehaviour
 {
 
     playerSave plrDb = new playerSave();
+    public GameObject ObjThrown;
 
     private void Start()
     {
         string file = File.ReadAllText(Application.dataPath + "playerSaveData.json");
         plrDb = JsonUtility.FromJson<playerSave>(file);
+        ObjThrown.SetActive(false);
     }
     // Update is called once per frame
     void Update()
     {
+        //When the Bro is 2 or 3 it makes the Stud Shoot
         if(BroSwitch.Bro == 2 && Input.GetMouseButtonDown(0) || BroSwitch.Bro == 3 && Input.GetMouseButtonDown(0))
         {
+            
             if(ToyCollect.toyAmount > 0)
             {
+               //Subtracting from the Total amount of toys collected 
                 ToyCollect.toyAmount--;
+                for (var i = 0; i < 10; i++)
+                {
+                    ObjThrown.SetActive(true);
+                    ObjThrown.transform.Translate(Vector3.right* Time.smoothDeltaTime* 10);
+                }
+                //Updating the Database
                 plrDb.collectedCharge = ToyCollect.toyAmount;
                 string toJson = JsonUtility.ToJson(plrDb);
                 JsonUtility.FromJsonOverwrite(toJson, plrDb);
